faa9f6dc8bf7d48c0f0a595be5c2504572b7b531
Jakub Bajtala <jakub.bajtala@gmail.com>
2017-06-26 11:28:18 +0200

    webpack, json, jsx filessetup

--- README.md
+++
@@ -1,1 +0,0 @@
-# cat-shelter-app
---
+++ jsx/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    ReactDOM.render(
+        <h1>Hello</h1>,
+        document.querySelector('#app')
+    );
+});
